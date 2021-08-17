ec7ce519086b10139ffdd0a46e3c73b22d424444
Justin <jmp86@yahoo.com>
2021-08-16 22:07:48 -1000

    commit tree 9ec92dd3b0d01b99466dc61101564d81bc972bc1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        array_copy = array
+        array_copy[-1] = "and #{array_copy[-1]}"
+        array_copy.join(', ')
+    end
 end
